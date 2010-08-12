c2259a07cf99eeaae81a56a6c08703d365b28035
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-13 01:31:06 +0800

    move to 0.3 series for clojure 1.2 development

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-yaml "0.2.0-SNAPSHOT"
-              :description      "YAML encoding(eventually) and decoding for Clojure using SnakeYAML"
-              :url              "http://github.com/lancepantz/clj-yaml"
-              :source-path      "src"
-              :dependencies     [[org.clojure/clojure "1.1.0"]
-                                 [org.clojure/clojure-contrib "1.1.0"]
-                                 [org.yaml/snakeyaml "1.5"]]
-              :repositories     [["snakeyaml" "http://snakeyamlrepo.appspot.com/repository"]]
-              :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+(defproject clj-yaml "0.3.0-SNAPSHOT"
+  :description "YAML encoding and decoding for Clojure using SnakeYAML"
+  :url "http://github.com/lancepantz/clj-yaml"
+  :dependencies
+    [[org.clojure/clojure "1.2.0-RC3"]
+     [org.clojure/clojure-contrib "1.2.0-RC3"]
+     [org.yaml/snakeyaml "1.5"]]
+  :repositories
+    [["snakeyaml" "http://snakeyamlrepo.appspot.com/repository"]])
