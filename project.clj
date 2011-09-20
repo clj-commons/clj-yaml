4bcad5a427e4f5f9fcd664a414e281828321293b
lance bradley <l@ncebradley.org>
2011-09-20 13:33:21 -0700

    use clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-yaml "0.3.0-SNAPSHOT"
+(defproject clj-yaml "0.3.1"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "http://github.com/lancepantz/clj-yaml"
   :dependencies
-    [[org.clojure/clojure "1.2.0-RC3"]
+    [[org.clojure/clojure "1.2.0"]
      [org.yaml/snakeyaml "1.5"]])
