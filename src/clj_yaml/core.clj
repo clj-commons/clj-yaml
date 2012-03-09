08418ea7616028b9c4c7d70a1b9a2876df34c68d
Max Penet <max.penet@gmail.com>
2012-03-09 13:53:39 +0100

    less redundant

--- src/clj_yaml/core.clj
+++ src/clj_yaml/core.clj
@@ -56,6 +56,6 @@
 (defn parse-string
   ([string keywordize]
      (binding [*keywordize* keywordize]
-       (decode (.load yaml string))))
+       (parse-string string)))
   ([string]
      (decode (.load yaml string))))
