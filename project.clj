a65a44f6b5836e7322a3ccd3d59d94e3f7b50418
tim dixon <tdixon51793@gmail.com>
2014-03-26 11:31:13 -0700

    -> circleci/clj-yaml

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-yaml "0.4.0"
+(defproject circleci/clj-yaml "0.4.0"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
-  :url "http://github.com/lancepantz/clj-yaml"
+  :url "https://github.com/circleci/clj-yaml"
   :dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.yaml/snakeyaml "1.13"]])
