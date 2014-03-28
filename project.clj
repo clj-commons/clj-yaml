f43761db11c2ed4d340dee2cdef0e2cf81f27620
tim dixon <tdixon51793@gmail.com>
2014-03-28 16:08:23 -0700

    :license field in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject circleci/clj-yaml "0.4.2"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/circleci/clj-yaml"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.yaml/snakeyaml "1.13"]])
