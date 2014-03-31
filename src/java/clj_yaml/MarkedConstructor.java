11e54d9f305ecf512a0ca5608c01cb439f2eb3cf
tim dixon <tdixon51793@gmail.com>
2014-03-31 14:45:35 -0700

    Refactored MarkedConstructor.

--- src/java/clj_yaml/MarkedConstructor.java
+++ src/java/clj_yaml/MarkedConstructor.java
@@ -1,5 +1,6 @@
 package clj_yaml;
 
+import org.yaml.snakeyaml.constructor.Construct;
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.constructor.SafeConstructor;
 import org.yaml.snakeyaml.constructor.AbstractConstruct;
@@ -12,23 +13,20 @@ import org.yaml.snakeyaml.error.Mark;
    end positions.
 */
 public class MarkedConstructor extends SafeConstructor {
+    /* The types we want to wrap. */
+    public static Tag[] tags = {Tag.NULL, Tag.BOOL, Tag.INT, Tag.FLOAT,
+                                Tag.BINARY, Tag.TIMESTAMP, Tag.OMAP,
+                                Tag.PAIRS, Tag.SET, Tag.STR, Tag.SEQ, Tag.MAP};
+
     public MarkedConstructor() {
         // Make sure SafeConstructor's constructor is called first,
         // so that we overwrite the keys that SafeConstructor sets.
         super();
-        this.yamlConstructors.put(Tag.NULL, new ConstructYamlNull());
-        this.yamlConstructors.put(Tag.BOOL, new ConstructYamlBool());
-        this.yamlConstructors.put(Tag.INT, new ConstructYamlInt());
-        this.yamlConstructors.put(Tag.FLOAT, new ConstructYamlFloat());
-        this.yamlConstructors.put(Tag.BINARY, new ConstructYamlBinary());
-        this.yamlConstructors.put(Tag.TIMESTAMP, new ConstructYamlTimestamp());
-        this.yamlConstructors.put(Tag.OMAP, new ConstructYamlOmap());
-        this.yamlConstructors.put(Tag.PAIRS, new ConstructYamlPairs());
-        this.yamlConstructors.put(Tag.SET, new ConstructYamlSet());
-        this.yamlConstructors.put(Tag.STR, new ConstructYamlStr());
-        this.yamlConstructors.put(Tag.SEQ, new ConstructYamlSeq());
-        this.yamlConstructors.put(Tag.MAP, new ConstructYamlMap());
-        this.yamlConstructors.put(null, undefinedConstructor);
+        // Wrap all the constructors with Marking constructors.
+        for (Tag tag : tags) {
+            Construct old = this.yamlConstructors.get(tag);
+            this.yamlConstructors.put(tag, new Marking(old));
+        }
     }
     /* An intermediate representation of data marked with start and
        end positions before we turn it into the nice clojure thing.
@@ -45,100 +43,19 @@ public class MarkedConstructor extends SafeConstructor {
         }
     }
 
-    public class ConstructYamlNull extends SafeConstructor.ConstructYamlNull {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
+    /* A wrapper around a Construct that marks source positions before calling
+       the original.
+     */
+    public class Marking extends AbstractConstruct {
+        public Construct constructor;
+        public Marking(Construct constructor) {
+            this.constructor = constructor;
         }
-    }
-    public class ConstructYamlBool extends SafeConstructor.ConstructYamlBool {
         public Object construct(Node node) {
             return new Marked
                 (node.getStartMark(),
                  node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlInt extends SafeConstructor.ConstructYamlInt {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlFloat extends SafeConstructor.ConstructYamlFloat {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlBinary extends SafeConstructor.ConstructYamlBinary {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlTimestamp extends SafeConstructor.ConstructYamlTimestamp {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlOmap extends SafeConstructor.ConstructYamlOmap {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlPairs extends SafeConstructor.ConstructYamlPairs {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlSet extends SafeConstructor.ConstructYamlSet {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlStr extends SafeConstructor.ConstructYamlStr {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlSeq extends SafeConstructor.ConstructYamlSeq {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
-        }
-    }
-    public class ConstructYamlMap extends SafeConstructor.ConstructYamlMap {
-        public Object construct(Node node) {
-            return new Marked
-                (node.getStartMark(),
-                 node.getEndMark(),
-                 super.construct(node));
+                 constructor.construct(node));
         }
     }
 }
