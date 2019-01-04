5871f10f35fcd4aac1d874cb2ffe124696369d39
Erik Assum <erik@assum.net>
2019-01-04 15:17:58 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject circleci/clj-yaml "0.6.0"
+(defproject clj-commons/clj-yaml "0.6.0"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
-  :url "https://github.com/circleci/clj-yaml"
+  :url "https://github.com/clj-commons/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
