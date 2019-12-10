a5f97faee4118dff93d35964b1acfc8c1ac52eb3
Stig Brautaset <stig@circleci.com>
2019-12-10 10:31:02 +0000

    Bump snakeyaml and Clojure dependencies

--- project.clj
+++ project.clj
@@ -11,6 +11,6 @@
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :dependencies
-  [[org.clojure/clojure "1.7.0"]
-   [org.yaml/snakeyaml "1.24"]
+  [[org.clojure/clojure "1.10.1"]
+   [org.yaml/snakeyaml "1.25"]
    [org.flatland/ordered "1.5.7"]])
