dd2da0b28d560dba2fe4c9c24435e8dc6ea0ec52
Erik Assum <erik@assum.net>
2020-01-17 23:06:24 +0100

    Capitalize letter

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as Clojure"}
+            :comments "Same as Clojure"}
   ;; Emit warnings on all reflection calls.
   :global-vars {*warn-on-reflection* true}
   :source-paths ["src/clojure"]
