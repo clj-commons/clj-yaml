090b507e51b1ad5992cb6525e2cc7739a401e601
Marc O'Morain <marc@circleci.com>
2020-09-01 10:44:39 +0100

    Prepare for 0.7.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-commons/clj-yaml "0.7.1"
+(defproject clj-commons/clj-yaml "0.7.2"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/clj-commons/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
