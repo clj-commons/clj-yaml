ee4e1cf7134487412ad11eb6e529f7ee56a932ee
Erik Assum <erik@assum.net>
2021-01-28 19:09:57 +0100

    Correctly specify version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-commons/clj-yaml (or (System/getenv "version") "0.7.2")
+(defproject clj-commons/clj-yaml (or (System/getenv "PROJECT_VERSION") "0.7.2")
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/clj-commons/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
