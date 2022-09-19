64d765a4480a0e3800d90dfa6f9e1f94509eab4a
lread <lee@dlread.com>
2022-09-19 11:24:44 -0400

    respond to review feedback
    missed one!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-commons/clj-yaml (or (System/getenv "PROJECT_VERSION") "0.7.4")
+(defproject clj-commons/clj-yaml (or (System/getenv "PROJECT_VERSION") "0.7.3")
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/clj-commons/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
