1d3a87efd410c9854dceb3e7038fec7f3b90d649
Lee Read <lee@dlread.com>
2022-09-27 14:03:07 -0400

    Automate publishing of release
    Invoked via `bb publish`.
    Read new doc/maintainer-guide.adoc for details.
    
    Other changes of note:
    - moved to GitHub Actions for CI
    - added CI test matrix for jdk 8, 11, 17 and ubuntu, windows
    - added bb tasks
    - completed transition from lein to deps.edn + build.clj
    
    Details of interest
    - adjust jdk 8 javac compatibility for jdk >= v9
    - using neil convention of storing version in deps.edn
    - tagging with v1.2.3 instead of Release-1.2.3
    - now at version 1.x (was 0.x)
    - version patch is now release count, next ver should be 1.0.26
    (was commit count)
      - clj-yaml - 3 releases
      - circleci/clj-yaml - 11 releases
      - clj-commons/clj-yaml - 11 releases
    - no longer gpg signing jars, we don't understand the value and don't
    want the complexity
    - temporarily left in project.clj, will follow up and delete after circleci build disabled

--- build.clj
+++ build.clj
@@ -1,26 +1,51 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [build-shared]
+            [clojure.java.shell :as shell]
+            [clojure.tools.build.api :as b]))
 
-(def lib 'clj-commons/clj-yaml)
-(def version (System/getenv "PROJECT_VERSION"))
+(def version (build-shared/lib-version))
+(def lib (build-shared/lib-artifact-name))
+
+;; build constants
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean [_]
-  (b/delete {:path "target"}))
+(defn- jdk-version
+  "Returns jdk version and major version with appropriate conversion. (ex 1.8 returns major of 8)"
+  []
+  (let [raw-version (->> (shell/sh "java" "-version")
+                         :err
+                         (re-find #"version \"(.*)\"")
+                         last)
+        major-minor (->> raw-version
+                         (re-find #"(\d+)(?:\.(\d+))?.*")
+                         rest
+                         (map #(when % (Integer/parseInt %))))]
+    {:version raw-version
+     :major (if (= (first major-minor) 1)
+              (second major-minor)
+              (first major-minor))}))
 
 (defn compile-java [_]
-  (b/javac {:src-dirs ["src/java"]
-            :class-dir class-dir
-            :basis basis
-            :javac-opts ["-source" "8" "-target" "8"]}))
+  (let [{:keys [version major]} (jdk-version)]
+    (println "compile-java with java version" version)
+    (when (< major 8)
+      (throw (ex-info "jdk version must be at least 8" {})))
+    (b/javac (cond-> {:src-dirs ["src/java"]
+                      :class-dir class-dir
+                      :basis basis}
+               (> major 8)
+               ;; replaces old jdk <= 8 -source and -target opts
+               (assoc :javac-opts ["--release" "8" "-Xlint"])))))
 
 (defn jar [_]
   (compile-java nil)
+  (println "jarring version" version)
   (b/write-pom {:class-dir class-dir
                 :lib lib
                 :version version
+                :scm {:tag (build-shared/version->tag version)}
                 :basis basis
                 :src-dirs ["src/clojure"]})
   (b/copy-dir {:src-dirs ["src/clojure"]
@@ -30,14 +55,16 @@
 
 (defn install [_]
   (jar {})
+  (println "installing version" version)
   (b/install {:basis basis
               :lib lib
-              :version version
+              :version version ;; can't remember why we need to repeat version here, it is in jar-file
               :jar-file jar-file
               :class-dir class-dir}))
 
 (defn deploy [opts]
   (jar opts)
+  (println "deploy")
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
    (merge {:installer :remote
            :artifact jar-file
